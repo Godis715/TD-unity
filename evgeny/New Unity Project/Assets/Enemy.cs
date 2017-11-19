0c17b9a6495a93c31276ebed4125425a332a0d03
Unknown <oleshkevich19990802@mail.ru>
2017-11-19 20:41:56 +0300

    сделали скрипт енеми

---
+++ evgeny/New Unity Project/Assets/Enemy.cs
@@ -0,0 +1,38 @@
+﻿
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public float speedOfEnemy = 10.0f;
+
+    private Transform target;
+    private int wayPointIndex = 0;
+
+    void Start()
+    {
+        target = WayPoints.points[wayPointIndex];
+    }
+
+    void Update ()
+    {
+        Vector3 direction = target.transform.position - this.transform.position;
+        transform.Translate(direction.normalized * speedOfEnemy * Time.deltaTime, Space.World);
+
+    if (Vector3.Distance(transform.position, target.position) <= 0.5f)
+        {
+            GetNextPoint();
+        }
+	}
+
+    void GetNextPoint()
+    {
+        if (wayPointIndex >= WayPoints.points.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        ++wayPointIndex;
+        target = WayPoints.points[wayPointIndex];
+    }
+}
--- evgeny/New Unity Project/Assets/WayPoints.cs
+++ evgeny/New Unity Project/Assets/WayPoints.cs
@@ -1,16 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿// using System.Collections;
+// using System.Collections.Generic;
 using UnityEngine;
 
 public class WayPoints : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public static Transform[] points;
+
+    void Awake ()
+    {
+        points = new Transform[transform.childCount];
+        for (int i = 0; i < points.Length; ++i)
+        {
+            points[i] = transform.GetChild(i);
+        }
+    }
 }
