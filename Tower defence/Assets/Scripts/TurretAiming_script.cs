953878373d35df0bc4929280d5eab7b173248c21
Nikita Rassudov <rassudov99@gmail.com>
2017-11-21 19:35:38 +0300

    Готовься, целься...
    Прописал башням скрипт захвата и удержания цели в пределах их зоны контроля. Следующий шаг - стрельба. Следите за обновлениями

---
+++ Tower defence/Assets/Scripts/TurretAiming_script.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurretAiming_script : MonoBehaviour
+{
+	private Transform target;
+
+	[Header("Turret parameters")]
+	public float range = 15.0f;
+	public float shootingFrequency = 10.0f;
+	public float damage = 1.0f;
+	public float rotationSpeed = 12.5f;
+
+	[Header("Other")]
+	public string enemyTag;
+	public GameObject rotateDirector;
+	public float enemySearchFrequency = 10.0f;
+
+	void Start()
+	{
+		InvokeRepeating("UpdateTarget", 0f, 1.0f / enemySearchFrequency);
+	}
+
+	void Update()
+	{
+		if(target == null)
+		{
+			return;
+		}
+
+		Vector3 arrowToEnemy = target.transform.position - this.transform.position;
+		Quaternion toRotate = Quaternion.LookRotation(arrowToEnemy);
+		Vector3 rotation = Quaternion.Lerp(rotateDirector.transform.rotation, toRotate, rotationSpeed * Time.deltaTime).eulerAngles;
+		rotateDirector.transform.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+	}
+	void UpdateTarget()
+	{
+		GameObject[] allEnemies = GameObject.FindGameObjectsWithTag(enemyTag);
+		GameObject nearestEnemy = null;
+		float minimalDistacne = Mathf.Infinity;
+		foreach(GameObject currentEnemy in allEnemies)
+		{
+			if(Vector3.Distance(this.transform.position, currentEnemy.transform.position) < minimalDistacne)
+			{
+				minimalDistacne = Vector3.Distance(this.transform.position, currentEnemy.transform.position);
+				nearestEnemy = currentEnemy;
+			}
+		}
+		if(nearestEnemy != null && minimalDistacne <= range)
+		{
+			target = nearestEnemy.transform;
+		}
+		else
+		{
+			target = null;
+		}
+	}
+
+	void OnDrawGizmosSelected()
+	{
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireSphere(this.transform.position, range);
+	}
+}
