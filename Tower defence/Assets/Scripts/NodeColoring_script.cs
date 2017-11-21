ac12e59ba82c522ae31b4de65c40bed66637c859
Nikita Rassudov <rassudov99@gmail.com>
2017-11-21 17:48:24 +0300

    Прогресс и развитие
    Прописан WaveSpawner, так что теперь имеем рабочий механизм спауна врагов. Так же нависил на ноды скрипт, окрашивающий их в нужный цвет при наведении на них мыши и щелчке по ним (Далее там должен быть более информативный интерфейс и графическая менюшка, но пока и так сойдет).

---
+++ Tower defence/Assets/Scripts/NodeColoring_script.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class NodeColoring_script : MonoBehaviour
+{
+	private Renderer rend;
+
+	[Header("Colors")]
+	public Color defaultColor;
+	public Color aimedColor;
+	public Color clickedColor;
+	public Color errorColor;
+
+	void Start()
+	{
+		rend = GetComponent<Renderer>();
+	}
+
+	void OnMouseEnter()
+	{
+		rend.material.color = aimedColor;
+	}
+
+	void OnMouseExit()
+	{
+		rend.material.color = defaultColor;
+	}
+
+	void OnMouseDown()
+	{
+		rend.material.color = clickedColor;
+	}
+}
---
+++ Tower defence/Assets/Scripts/WaveSpawner_script.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaveSpawner_script : MonoBehaviour
+{
+    [Header("Spawning")]
+    public float timeBetweenWaves = 5.0f;
+    public GameObject enemyPrefab;
+    public Transform spawnPoint;
+
+    private float countdownToWave = 5.0f;
+    private int waveIndex = 0;
+
+    void Update()
+    {
+        countdownToWave -= Time.deltaTime;
+        if(countdownToWave <= 0f)
+        {
+            waveIndex++;
+            StartCoroutine(SpawnWave());
+            countdownToWave = timeBetweenWaves;
+        }
+    }
+
+    IEnumerator SpawnWave()
+    {
+        for (int i = 1; i <= waveIndex; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.transform.position, spawnPoint.transform.rotation);
+    }
+}
