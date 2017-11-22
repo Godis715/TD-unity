c1a00a8aa42d651f8ee57f3ae304e7ffcdda5b0f
Tereschenko Dmitry <dima123-99@mail.ru>
2017-11-22 22:33:55 +0300

    ScriptTurret

--- DimaT_game/Assets/Enemy.cs
+++ DimaT_game/Assets/Enemy.cs
@@ -11,13 +11,11 @@ public class Enemy : MonoBehaviour {
     {
         target = Waypoits.points[0];
     }
-
-    void Update()
+	void Update()
     {
-        Vector3 dir = target.position - transform.position;
-        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
-
-        if (Vector3.Distance(target.position, transform.position) <= 0.4f)
+		Vector3 dir = target.position - transform.position;
+		transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+		if (Vector3.Distance(target.position, transform.position) <= 0.4f)
         {
             GetNextWaypoint();
         }
