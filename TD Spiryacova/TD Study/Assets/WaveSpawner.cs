2a156df12995f976f0c1af099b67159bfc12d0bf
Unknown <notavailablenow@mail.ru>
2017-11-22 10:56:31 +0300

    Added enemies(just walking along the path)

---
+++ TD Spiryacova/TD Study/Assets/WaveSpawner.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour {
+    public Transform enemyPrefab;
+
+    public float timeBetweenWaves = 5f;
+    private float countdown = 2f;
+
+    private int waveNumber = 1;
+
+    void Update()
+    {
+        if (countdown <= 0)
+        {
+            SpawnWave();
+            countdown = timeBetweenWaves;
+        }
+        countdown -= Time.deltaTime;
+    }
+
+    void SpawnWave()
+    {
+        Debug.Log("Wave incoming");
+    }
+
+}
---
+++ TD Spiryacova/TD Study/Assets/Waypoints.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Waypoints : MonoBehaviour
+{
+    public static Transform[] points;
+
+    void Awake()
+    {
+        points = new Transform[this.transform.childCount];
+        for (int i = 0; i < points.Length; ++i)
+        {
+            points[i] = this.transform.GetChild(i);
+        }
+    }
+	
+}
