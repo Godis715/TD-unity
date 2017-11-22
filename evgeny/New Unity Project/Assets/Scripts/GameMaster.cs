46ff1926e5fc175947edd7025283fd7909e768f8
Unknown <oleshkevich19990802@mail.ru>
2017-11-22 12:34:38 +0300

    загрузил банни и спаун волн

---
+++ evgeny/New Unity Project/Assets/Scripts/GameMaster.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour
+{
+
+	public Transform EnemyPrefab;
+	public Transform SpownPoint;
+
+	public float TimeBetvenWaves = 5.0f;
+	public Text CountDownText;
+
+	private float CountDown = 2.0f;
+	private int WavesNumber = 0;
+
+	void Update() {
+		if (CountDown <= 0.0f)
+		{
+			StartCoroutine(SpownWaves());
+			CountDown = TimeBetvenWaves + WavesNumber / 2;
+		}
+
+		CountDown -= Time.deltaTime;
+
+		CountDownText.text = Mathf.Floor(CountDown).ToString();
+		return;
+	}
+	IEnumerator SpownWaves() {
+		++WavesNumber;
+		for (int i = 1; i <= WavesNumber; ++i)
+		{
+			SpownEnemy();
+			yield return new WaitForSeconds(0.4f);
+		}
+	}
+
+	void SpownEnemy()
+	{
+		Instantiate(EnemyPrefab, SpownPoint.position, SpownPoint.rotation);
+		return;
+	}
+}
