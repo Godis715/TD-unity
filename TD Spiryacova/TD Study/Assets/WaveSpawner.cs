07c27a6ff6fb9f38740068d404deb11452d84503
Unknown <notavailablenow@mail.ru>
2017-11-22 12:04:49 +0300

    Lesson 3 complete

--- TD Spiryacova/TD Study/Assets/WaveSpawner.cs
+++ TD Spiryacova/TD Study/Assets/WaveSpawner.cs
@@ -1,26 +1,45 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class WaveSpawner : MonoBehaviour {
-    public Transform enemyPrefab;
 
-    public float timeBetweenWaves = 5f;
-    private float countdown = 2f;
+	[Header("Spawner")]
+	public Transform enemyPrefab;
+	public Transform spawnPoint;
 
-    private int waveNumber = 1;
+	public float timeBetweenWaves = 5.5f;
+	private float countdown = 2f;
 
-    void Update()
-    {
-        if (countdown <= 0)
-        {
-            SpawnWave();
-            countdown = timeBetweenWaves;
-        }
-        countdown -= Time.deltaTime;
-    }
+	public Text waveCountdown;
+	private int waveIndex = 0;
 
-    void SpawnWave()
-    {
-        Debug.Log("Wave incoming");
-    }
+	void Update()
+	{
+		if (countdown <= 0)
+		{
+			StartCoroutine(SpawnWave());
+			countdown = timeBetweenWaves;
+		}
+		countdown -= Time.deltaTime;
+		waveCountdown.text = Mathf.Round(countdown).ToString();
+	}
+
+	IEnumerator SpawnWave()
+	{
+		++waveIndex;
+		
+		for (int i = 0; i < waveIndex; ++i)
+		{
+			SpawnEnemy();
+			yield return new WaitForSeconds(0.4f);
+		}
+		
+	}
+
+	void SpawnEnemy()
+	{
+		Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+	}
 
 }
--- TD Spiryacova/TD Study/Assets/Waypoints.cs
+++ TD Spiryacova/TD Study/Assets/Waypoints.cs
@@ -2,15 +2,14 @@
 
 public class Waypoints : MonoBehaviour
 {
-    public static Transform[] points;
+	public static Transform[] points;
 
-    void Awake()
-    {
-        points = new Transform[this.transform.childCount];
-        for (int i = 0; i < points.Length; ++i)
-        {
-            points[i] = this.transform.GetChild(i);
-        }
-    }
-	
+	void Awake()
+	{
+		points = new Transform[transform.childCount];
+		for (int i = 0; i < points.Length; ++i)
+		{
+			points[i] = transform.GetChild(i);
+		}
+	}
 }
