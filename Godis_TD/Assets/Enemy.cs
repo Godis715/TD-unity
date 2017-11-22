c636c97d4e77156e58a4c5849897e508c5f236e3
Denis Ignatev <godis7.15@gmail.com>
2017-11-22 10:11:51 +0300

    The lesson 2 complete

---
+++ Godis_TD/Assets/Enemy.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public float speed = 10f;
+
+    private Transform target;
+    private int wavepointIndex = 0;
+
+    void Start() {
+        target = Waypoints.points[0];
+    }
+
+    void Update() {
+        Vector3 dir = target.position - this.transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, target.position) < 0.4f) {
+            getNextWaypoint();
+        }
+    }
+
+    void getNextWaypoint()
+    {
+        if (wavepointIndex >= Waypoints.points.Length - 1) {
+            Destroy(this.gameObject);
+            return;
+        }
+        wavepointIndex++;
+        target = Waypoints.points[wavepointIndex];
+    }
+}
